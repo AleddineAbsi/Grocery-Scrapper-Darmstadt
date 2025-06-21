33dcd586e01da74735c5bca9dce87f7c2508977b
Aleddine Absi <aleddine.absi1@gmail.com>
2025-06-21 20:38:06 +0200

    test jsoup

--- grocery-scraper/build.gradle.kts
+++ grocery-scraper/build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.jsoup:jsoup:1.16.1")
+    implementation("org.seleniumhq.selenium:selenium-java:4.21.0")
 }
 
 tasks.test {
--- grocery-scraper/src/main/java/com/aleddineabsi/scrapper/Main.java
+++ grocery-scraper/src/main/java/com/aleddineabsi/scrapper/Main.java
@@ -1,8 +1,27 @@
 package com.aleddineabsi.scrapper;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        try{
+            String url="https://books.toscrape.com/";
 
+            Document doc = Jsoup.connect(url).get();
+
+            Elements Items = doc.select(".product_pod");
+            System.out.println("Livres trouvees :");
+
+            for(Element item : Items){
+                String bookTitle = item.selectFirst("h3 a").attr("title");
+                String bookPrice = item.selectFirst(".price_color").text();
+                System.out.println("- "+ bookTitle + " = " + bookPrice);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
